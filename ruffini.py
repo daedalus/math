aad337c08d8fc648c325c0a6acf5fd7c12f8cb90
dclavijo <daedalus@users.noreply.github.com>
2015-07-04 00:22:17 -0300

    ruffini rule
    This small test will derive all the roots of a poli in n-grade stoping at the 2-grade poli

---
+++ ruffini.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#
+# Author Dario Clavijo 2015
+# GPLv3
+
+from math import *
+
+# tells if a n is prime or not
+def isPrime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+
+    return True
+
+# finds the prime factors of n
+def factors(n):
+   factor = []
+   limit = int(round(sqrt(n), 2)) + 1
+   check = 2
+   num = n
+   if isPrime(n):
+      return [n]
+   for check in range(2, limit):
+      if isPrime(check) and (num % check) == 0:
+         factor.append(check)
+         num /= check
+         if isPrime(num):
+            factor.append(num)
+            break
+   factor = sorted(factor)
+   return factor
+
+#add the negative factors to try
+def addnegatives(D):
+	nD = []
+	for i in D:
+		nD.append(-abs(i))
+		nD.append(abs(i))
+	return nD
+
+# one step ruffini
+def ruffini_step(P,D):
+
+	for i in range(len(D)):
+		tmpPoli = []
+		for j in range(len(P)):
+			if j == 0:
+				tmpPoli.append(P[j])
+			else:
+				tmpPoli.append(P[j] + (D[i]*tmpPoli[j-1]))
+		if tmpPoli[len(tmpPoli)-1]==0: 
+			return [D[i],tmpPoli]
+
+def ruffini_test(P):
+	print "P=",P
+	TI = P[len(P)-1]
+	D = [1] + factors(abs(TI)) + [abs(TI)]
+	D = addnegatives(D)
+	D.sort()
+	print "D=", D
+
+	tmp = P
+	for k in range(len(P)-3):
+		ret = ruffini_step(tmp,D)
+		tmp = ret[1]
+		print "Grade=",len(P)-k,"root=",ret[0],"coefs=",ret[1]
+
+#P = [1,-7,13,23,-78]
+P = [6,-29,27,27,-29,6]
+ruffini_test(P)
