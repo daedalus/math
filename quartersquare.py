aa268a2b9cfd6dd6de6daac974c52e77ae3fecb5
Darío Clavijo <daedalus2027@gmail.com>
2015-07-09 22:47:17 -0300

    quarter square and log mult

---
+++ quartersquare.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import math
+
+def qsqr(a,b):
+	return (((a+b)**2)/4) - (((a-b)**2)/4)
+
+def logmult(a,b):
+	return math.e ** (math.log(a)+math.log(b))
+
+a = 255
+b = 255
+print logmult(a,b)
+print qsqr(a,b)
+print a*b
+
+
