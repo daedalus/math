f1e242f1915153b9b6d4b2666df36df230884adb
Darío Clavijo <dclavijo@antel.net.uy>
2015-07-23 14:44:41 -0300

    missing prosthaphaeresis identities

--- prosthaphaeresis.py
+++ prosthaphaeresis.py
@@ -5,16 +5,41 @@
 
 import math
 
-def pmult(a,b):
+def pmultcos(a,b):
 
 	a = math.acos(a)
 	b = math.acos(b)
 	
 	return (math.cos(a+b) + math.cos(a-b))/2
 
+
+def pmultsin(a,b):	
+
+	a = math.asin(a)
+	b = math.asin(b)
+	
+	return (math.cos(a-b) - math.cos(a+b))/2
+
+def pmultsincos(a,b):
+	
+	a = math.asin(a)
+	b = math.acos(b)
+
+	return (math.sin(a+b) + math.sin(a-b))/2
+
+def pmultcossin(a,b):
+
+	a = math.acos(a)	
+	b = math.asin(b)
+
+	return (math.sin(a+b) - math.sin(a-b))/2
+
 a = 0.17
 b = 0.37
-print pmult(a,b)
+
 print a*b
+print pmultcos(a,b)
+print pmultsin(a,b)
+print pmultsincos(a,b)
 
 
