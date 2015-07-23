57628974a1fd9091d288273451d54b2f9e00ed50
dclavijo <daedalus@users.noreply.github.com>
2015-07-23 15:04:20 -0300

    abs representation

--- sign.py
+++ sign.py
@@ -3,9 +3,19 @@
 # Author Dario Clavijo 2015
 # GPLv3
 
+def _abs(x):
+        if x >= 0:
+                return x
+        else:
+                return -x
+
 # sign of x returns: [-1,1]
 def sign(x):
     if x == 0:
         return 0
     else:
-        return x/abs(x)
+        return x/_abs(x)
+
+print sign(1)
+print sign(0)
+print sign(-1)
