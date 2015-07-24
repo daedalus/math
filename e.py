10fbd964820f5deb99680b4bf22e95d9a2529fc9
Darío Clavijo <dclavijo@antel.net.uy>
2015-07-24 16:02:20 -0300

    taylor aproximation of e

--- e.py
+++ e.py
@@ -6,10 +6,18 @@
 import math
 
 # the x magic number is between 6444429920 and 6444429921
-def e():
+def direct_e():
         x = 6444429920 + 0.22 # or int(0x1801e3260,16) + 0.22
         ret = (1+(1.0/x))**x
         return ret
 
+
+def taylor_e():
+	accum = 0
+	for n in xrange(0,15):
+		accum += 1.0/(math.factorial(n))
+	return accum
+
 print math.e
-print e()
+print direct_e()
+print taylor_e()
