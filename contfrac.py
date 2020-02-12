cba42cbbc9b5ab79198ffd512b9f02a86e1d21d6
Darío Clavijo <clavijodario@gmail.com>
2020-02-12 00:09:54 -0300

    Contined fraction example

---
+++ contfrac.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# https://en.wikipedia.org/wiki/Continued_fraction
+# Author Dario Clavijo 2020
+# GPLv3
+
+def cont_frac(a,b):
+  coef=[]
+  i = a//b
+  while b != 0:
+    r = a//b
+    i = r
+    coef.append(i)
+    f = a - i*b
+    a = b
+    b = f
+  return coef
+
+print (cont_frac(649,200))
+print (cont_frac(17993,90581))
