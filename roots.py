704e4d632ee394a221624e2b518c3e1c2816b2e1
Darío Clavijo <clavijodario@gmail.com>
2020-04-06 00:15:00 -0300

    nth-root of x with log

---
+++ roots.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Author Dario Clavijo 2020
+# nth root of x
+
+import math
+
+def nth_root(n,x):
+  return math.e ** ((1.0/n) * math.log(x))
+
+print nth_root(2,2)
+print nth_root(3,8)
+
