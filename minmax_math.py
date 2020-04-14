bf16e0dd3df633f6a2bc10b6b6b2f114829a7258
Dario Clavijo <clavijodario@gmail.com>
2020-04-14 20:20:15 -0300

    arithmetric min and max functions

---
+++ minmax_math.py
@@ -0,0 +1,13 @@
+#Author Dario Clavijo 2020
+#GPLv3
+import math
+
+def _min(a,b):
+  return 0.5 * (a+b - math.sqrt((a-b)**2))
+
+def _max(a,b):
+  return 0.5 * (a+b + math.sqrt((a-b)**2))
+
+print _min(5,7)
+print _max(5,7)
+
