64aa5053ca25a6c6adee715b0b816cecb8e0449f
Dario Clavijo <clavijodario@gmail.com>
2020-12-22 16:00:08 -0200

    add powmod.py

---
+++ powmod.py
@@ -0,0 +1,21 @@
+from gmpy2 import next_prime
+
+def powmod(base, exponent, modulus):
+    if modulus == 1:
+        return 0
+    c = 1
+    e_prime = 0
+    while e_prime < exponent:
+        c = (c * base) % modulus
+        e_prime +=1
+    return c
+
+def test(A):
+ print(A)
+ a = powmod(A[0],A[1],A[2])
+ b = pow(A[0],A[1],A[2])
+ print(a,b)
+ return a == b
+
+print(test((13,484,497)))
+
