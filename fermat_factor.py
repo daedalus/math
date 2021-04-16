9f4f5275ba81e01695b0ec5b711fb974df015f75
Darío Clavijo <clavijodario@gmail.com>
2021-04-16 18:25:05 -0300

    Update fermat_factor.py

--- fermat_factor.py
+++ fermat_factor.py
@@ -6,21 +6,21 @@
 import gmpy2
 
 def fermat(n):
-	a = int(gmpy2.ceil(gmpy2.isqrt(n)))
-	b2 = (a**2) -n
-	step=0
-	while not gmpy2.is_square(b2):
-		a += 1
-		b2 = (a**2) -n
-		step+=1
-		print(n,step,a,b2)
-	return a-gmpy2.isqrt(b2),a + gmpy2.isqrt(b2)
+  a = gmpy2.isqrt(n)
+  b2 = (a**2) -n
+  step=0
+  while not gmpy2.is_square(b2):
+    a += 1
+    b2 = (a**2) -n
+    step+=1
+    print(n,step,a,b2)
+  return a-gmpy2.isqrt(b2),a + gmpy2.isqrt(b2)
 
 def test():
-	n=5959
-	print ("-"*40)
-	a,b= fermat(n)
-	print ("-"*40)
-	print (n,a,b,n==a*b)
+  n=5959
+  print ("-"*40)
+  a,b= fermat(n)
+  print ("-"*40)
+  print (n,a,b,n==a*b)
 
 test()
