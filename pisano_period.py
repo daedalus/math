b8e500327a1023c460b7e9d4166a00f14e3f1af6
Darío Clavijo <clavijodario@gmail.com>
2021-04-21 12:57:23 -0300

    Create pisano_period.py

---
+++ pisano_period.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Author Dario Clavijo 2021
+import sys
+
+def pisano_period(m):
+  if m == 1:
+    return 1
+  a = b = 1
+  n = 0
+  while True:
+    b,a = a,(a+b) % m
+    if a == 1 and b ==0:
+      return n + 2
+    n+=1
+
+if __name__ == "__main__":
+  for i in range(1,100):
+    print(pisano_period(i))
