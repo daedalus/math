9eb3a4baf23d78697231e2a64edc9879ad5119e6
Darío Clavijo <clavijodario@gmail.com>
2021-04-28 22:05:06 -0300

    Cyclic Group G of order n with a generator g (Z/nZ)*

--- multiplicativeOrder.py
+++ multiplicativeOrder.py
@@ -21,10 +21,21 @@ def ZnMultGroup(n):
     i+=1
   return tmp
 
+def CyclicGroup(g,n):
+  """ Cyclic Group G of order n with a generator g (Z/nZ)* """
+  i=1
+  tmp = []
+  while i<n:
+    tmp.append(pow(g, i, n))
+    i += 1
+  return tmp
+
 if __name__ == "__main__":
   print("ZnMultGroup(10)",ZnMultGroup(10))
   print("ZnMultGroup(11)",ZnMultGroup(11))
+  print("CyclicGroup(2,11)",CyclicGroup(2,11))
   for i in range(2,10):
     for j in range(i,10):
       if gcd(i,j) == 1:
+        #print("Zn:",j,ZnMultGroup(j))
         print("multiplicativeOrder",i,j,multiplicativeOrder(i,j))
