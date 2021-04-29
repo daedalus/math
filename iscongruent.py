93d065159f0738715fdf808706ea2cacdfbd4d0c
Darío Clavijo <clavijodario@gmail.com>
2021-04-28 22:35:16 -0300

    add iscongruent.py

---
+++ iscongruent.py
@@ -0,0 +1,8 @@
+def iscongruent(a,b,n):
+    """ a and b are congruent only if the absolute difference of both divides n"""
+    return (abs(a-b) // n )  == 0
+
+if __name__ == "__main__":
+    print(iscongruent(4,6,12))
+    print(iscongruent(5,12,60))
+
