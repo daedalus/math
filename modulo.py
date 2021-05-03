a791254b24e7e02d031e82299c7eb0d06fcf7756
Darío Clavijo <clavijodario@gmail.com>
2021-05-03 16:35:34 -0300

    add divMod

--- modulo.py
+++ modulo.py
@@ -6,12 +6,21 @@ def subMod(a,n):
         n -= a
     return n
 
+def divMod(d,n):
+    """ Naive division modulo implementation with substraction """
+    r = 0
+    while d <= n:
+        n -= d
+        r += 1
+    return r,n
+
 def test():
     x = 10
     for i in range(1,11):
         a = subMod(i,x)
+        r,m = divMod(i,x)
         b = x % i
-        print(x,i,a,b,a==b)
+        print(x,i,a,b,a==b,r,m, x == (r*i)+m)
 
 
 if __name__ == "__main__":
