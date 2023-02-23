c014bc1192de3162147f0ad00b3b1725b7e1b215
dclavijo <clavijodario@gmail.com>
2023-02-23 12:51:46 -0300

    add FormalDerivative

---
+++ FormalDerivative.py
@@ -0,0 +1,12 @@
+def FormalDerivative(Fx):
+  return [(x[0] * x[1], x[1] - 1) for x in Fx if (x[1]-1) > -1]
+
+
+def polyrep(Fx):
+  s = ["%dx ^ %d" % x for x in Fx]
+  return " + ".join(s)
+
+fx = [(1,6),(1,0)]
+print(polyrep(fx))
+fx = FormalDerivative(fx)
+print(polyrep(fx))
