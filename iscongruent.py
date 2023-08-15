c1793d53385aacb5dcb1ab97c4d93a4eefb1d60c
Darío Clavijo <clavijodario@gmail.com>
2023-08-15 15:18:05 -0300

    Update iscongruent.py

--- iscongruent.py
+++ iscongruent.py
@@ -1,6 +1,7 @@
 def iscongruent(a, b, n):
     """a and b are congruent modulo n only if the absolute difference of both divides n"""
-    return (abs(a - b) // n) == 0
+    #return (abs(a - b) // n) == 1
+    return (abs(a - b) % n) == 0
 
 
 if __name__ == "__main__":
